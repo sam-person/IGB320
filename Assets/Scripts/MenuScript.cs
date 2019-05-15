44218aa9051a7c3b03010571e8fa59de967c4011
MaltyCrab <maltycrab@gmail.com>
2019-05-15 15:32:54 +1000

    Added side menu
    made additions to the menu screen
    an animation now plays when the button is pressed

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+
+    public GameObject sideMenu; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void moveSideMenu()
+    {
+
+    }
+
+}
