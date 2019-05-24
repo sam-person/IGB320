cbb7ca6001484f82ddd432c0106f44beada1e718
MaltyCrab <maltycrab@gmail.com>
2019-05-24 16:52:11 +1000

    added all menu screens
    made connections to most menus for the (average) user

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -5,13 +5,26 @@ using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
+    public GameObject mainMenuCanvas;
+    public GameObject homeCanvas;
+    public GameObject shopMenu;
+    //public GameObject inventoryMenu;
+    public GameObject helpMenu;
 
-    public GameObject sideMenu; 
+    GameObject homeMenuButtonCanvas;
+    GameObject bagsMenu;
+
+    GameObject homeSprite;
+    GameObject savingsSprite;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        homeMenuButtonCanvas = homeCanvas.transform.Find("homeMenu").gameObject;
+        bagsMenu = homeCanvas.transform.Find("bagsMenu").gameObject;
+
+        homeSprite = homeCanvas.transform.Find("homeSprite").gameObject;
+        savingsSprite = homeCanvas.transform.Find("savingsSprite").gameObject;
     }
 
     // Update is called once per frame
@@ -20,9 +33,57 @@ public class MenuScript : MonoBehaviour
         
     }
 
-    void moveSideMenu()
+    public void login()
     {
+        //remove main menu screen
+        mainMenuCanvas.SetActive(false);
 
+        //activate home screen
+        homeCanvas.SetActive(true);
+        homeMenuButtonCanvas.SetActive(true);
     }
+
+    public void openShop()
+    {
+        //remove home screen
+        homeSprite.SetActive(false);
+        homeMenuButtonCanvas.SetActive(false);
+
+        //activate shop screen
+        shopMenu.SetActive(true);
+    }
+
+    public void openBags()
+    {
+        //remove home screen
+        homeSprite.SetActive(false);
+        homeMenuButtonCanvas.SetActive(false);
+
+        //activate bags screen
+        bagsMenu.SetActive(true);
+    }
+
+    public void openHelp()
+    {
+        //remove home screen
+        homeSprite.SetActive(false);
+        homeMenuButtonCanvas.SetActive(false);
+
+        //activate help screen
+        helpMenu.SetActive(true);
+    }
+
+    public void backHome()
+    {
+        //remove current screen
+        shopMenu.SetActive(false);
+        bagsMenu.SetActive(false);
+        helpMenu.SetActive(false);
+
+        //activate home screen
+        homeSprite.SetActive(true);
+        homeMenuButtonCanvas.SetActive(true);
+    }
+
 
 }
